2d9c863fb1da2b1b885d124a91b442e661b2a6e5
Saikat Sinha Ray <saikat.sinharay@gmail.com>
2020-04-19 21:08:22 +0530

    Update 1-1.py

--- solutions/hash_table/1-1.py
+++ solutions/hash_table/1-1.py
@@ -15,18 +15,21 @@ class hash_table_class(object):
 
 		for char_from_string in string:
 			hash_value=self.calculate_hash(char_from_string)
-
+			
+			# If a char already exists on this location means the string having a duplication char. No need to check further
 			if self.hash_table[hash_value] != None:
 				print("There are duplicate characters")
 				return
 			
 			self.hash_table[hash_value]=char_from_string
-		
+
+		# Control rechaed here means there is no duplicate chars
 		print("All character are unique")
 
 print("Please enter the ASCII string")
 string=input()
 
+# Total characters in ASCII is 128. So, If a string contains more than 128 chars then there are duplicates
 if len(string) > 128:
 	print("There are duplicate characters")
 
